0dba3b4867a0982badc7db7a6488202d4b79c15f
Biddulph, Caleb D <130435@s020rmc148-21.local>
2018-12-05 14:18:24 -0600

    Initial Commit

---
+++ Connect 4/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Connect 4
+//
+//  Created by Biddulph, Caleb D on 12/5/18.
+//  Copyright © 2018 Connect 4 Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
